462ddd152c0b1ac071feb593af076c926346d0be
Georgi Kanchev <gogata.199999@gmail.com>
2026-08-18 03:03:24 +0300

    More sounds + music; Flag shadows

--- game/audio.go
+++ game/audio.go
@@ -13,39 +13,39 @@ var AudioBow, AudioProjectileGround, AudioProjectileWood, AudioProjectileMetal, 
 var AudioHitFlesh, AudioHitWood, AudioHitMetal []audio.Audio
 
 func LoadAudio() {
-	ambiences[EnvironmentPlains] = assets.LoadMusic("data/audio/ambience-plains.mp3")
+	ambiences[EnvironmentPlains] = assets.LoadMusic("data/music/ambience-plains.mp3")
 	AudioAmbience = audio.New(ambiences[EnvironmentPlains])
 
-	AudioDoorOpen = append(AudioDoorOpen, audio.New(assets.LoadSound("data/audio/door-open1.mp3", 4)))
-	AudioDoorOpen = append(AudioDoorOpen, audio.New(assets.LoadSound("data/audio/door-open2.mp3", 4)))
-	AudioDoorClose = append(AudioDoorClose, audio.New(assets.LoadSound("data/audio/door-close1.mp3", 4)))
-	AudioDoorClose = append(AudioDoorClose, audio.New(assets.LoadSound("data/audio/door-close2.mp3", 4)))
-	AudioGateOpen = append(AudioGateOpen, audio.New(assets.LoadSound("data/audio/gate-open1.mp3", 4)))
-	AudioGateClose = append(AudioGateClose, audio.New(assets.LoadSound("data/audio/gate-close1.mp3", 4)))
-	AudioGateClose = append(AudioGateClose, audio.New(assets.LoadSound("data/audio/gate-close2.mp3", 4)))
+	AudioDoorOpen = append(AudioDoorOpen, audio.New(assets.LoadSound("data/sounds/door-open1.mp3", 4)))
+	AudioDoorOpen = append(AudioDoorOpen, audio.New(assets.LoadSound("data/sounds/door-open2.mp3", 4)))
+	AudioDoorClose = append(AudioDoorClose, audio.New(assets.LoadSound("data/sounds/door-close1.mp3", 4)))
+	AudioDoorClose = append(AudioDoorClose, audio.New(assets.LoadSound("data/sounds/door-close2.mp3", 4)))
+	AudioGateOpen = append(AudioGateOpen, audio.New(assets.LoadSound("data/sounds/gate-open1.mp3", 4)))
+	AudioGateClose = append(AudioGateClose, audio.New(assets.LoadSound("data/sounds/gate-close1.mp3", 4)))
+	AudioGateClose = append(AudioGateClose, audio.New(assets.LoadSound("data/sounds/gate-close2.mp3", 4)))
 	for i := 1; i < 5; i++ {
-		var door = assets.LoadSound(text.New("data/audio/door-crumble", i, ".mp3"), 4)
-		var gate = assets.LoadSound(text.New("data/audio/gate-crumble", i, ".mp3"), 4)
+		var door = assets.LoadSound(text.New("data/sounds/door-crumble", i, ".mp3"), 4)
+		var gate = assets.LoadSound(text.New("data/sounds/gate-crumble", i, ".mp3"), 4)
 		AudioDoorCrumble = append(AudioDoorCrumble, audio.New(door))
 		AudioGateCrumble = append(AudioGateCrumble, audio.New(gate))
 	}
 
-	AudioHitWood = append(AudioHitWood, audio.New(assets.LoadSound("data/audio/hit-wood1.mp3", 4)))
-	AudioHitWood = append(AudioHitWood, audio.New(assets.LoadSound("data/audio/hit-wood2.mp3", 4)))
+	AudioHitWood = append(AudioHitWood, audio.New(assets.LoadSound("data/sounds/hit-wood1.mp3", 4)))
+	AudioHitWood = append(AudioHitWood, audio.New(assets.LoadSound("data/sounds/hit-wood2.mp3", 4)))
 	for i := 1; i < 6; i++ {
-		AudioHitFlesh = append(AudioHitFlesh, audio.New(assets.LoadSound(text.New("data/audio/hit-flesh", i, ".mp3"), 4)))
+		AudioHitFlesh = append(AudioHitFlesh, audio.New(assets.LoadSound(text.New("data/sounds/hit-flesh", i, ".mp3"), 4)))
 	}
 	for i := 1; i < 4; i++ {
-		AudioHitMetal = append(AudioHitMetal, audio.New(assets.LoadSound(text.New("data/audio/hit-metal", i, ".mp3"), 4)))
+		AudioHitMetal = append(AudioHitMetal, audio.New(assets.LoadSound(text.New("data/sounds/hit-metal", i, ".mp3"), 4)))
 	}
 
-	AudioBow = append(AudioBow, audio.New(assets.LoadSound("data/audio/bow.mp3", 4)))
-	AudioProjectileGround = append(AudioProjectileGround, audio.New(assets.LoadSound("data/audio/projectile-ground.mp3", 4)))
-	AudioProjectileWood = append(AudioProjectileWood, audio.New(assets.LoadSound("data/audio/projectile-wood.mp3", 4)))
-	AudioProjectileMetal = append(AudioProjectileMetal, audio.New(assets.LoadSound("data/audio/projectile-metal1.mp3", 4)))
-	AudioProjectileMetal = append(AudioProjectileMetal, audio.New(assets.LoadSound("data/audio/projectile-metal2.mp3", 4)))
+	AudioBow = append(AudioBow, audio.New(assets.LoadSound("data/sounds/bow.mp3", 4)))
+	AudioProjectileGround = append(AudioProjectileGround, audio.New(assets.LoadSound("data/sounds/projectile-ground.mp3", 4)))
+	AudioProjectileWood = append(AudioProjectileWood, audio.New(assets.LoadSound("data/sounds/projectile-wood.mp3", 4)))
+	AudioProjectileMetal = append(AudioProjectileMetal, audio.New(assets.LoadSound("data/sounds/projectile-metal1.mp3", 4)))
+	AudioProjectileMetal = append(AudioProjectileMetal, audio.New(assets.LoadSound("data/sounds/projectile-metal2.mp3", 4)))
 	for i := 1; i <= 7; i++ {
-		var asset = assets.LoadSound(text.New("data/audio/projectile-flesh", i, ".mp3"), 4)
+		var asset = assets.LoadSound(text.New("data/sounds/projectile-flesh", i, ".mp3"), 4)
 		AudioProjectileFlesh = append(AudioProjectileFlesh, audio.New(asset))
 	}
 }
